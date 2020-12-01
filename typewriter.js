dd8ddd2bd542f1e1c8c09ead5598366f409157b1
luke-berzins <lukeberzins16@gmail.com>
2020-12-01 14:47:58 +0000

    typewriter

---
+++ typewriter.js
@@ -0,0 +1,13 @@
+const sentence = "hello there from lighthouse labs";
+
+let timer = 0
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char)
+    console.log(' => ' + timer + ' ms')
+    timer += 50;
+  }, timer) 
+}
+setTimeout(() => {
+  console.log("\n")
+}, timer)
